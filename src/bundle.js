9d5bf6fab0b8d8b190a4ea189a2f4f12d59fff5f
Benjamin Hodges <benjaminhodges07@gmail.com>
2019-07-22 15:13:44 +0100

    Working on click

--- src/bundle.js
+++ src/bundle.js
@@ -1,52 +1,89 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-var d3 = require("d3"),
-    cloud = require("d3-cloud");
+var d3 = require("d3");
+var cloud = require("d3-cloud");
 
+var papers = {}
 
-fetch('freq_data.csv')
-  .then(response => response.text())
-  .then((data) => {
-	  arr = data.split("\n");
-	  words = []
-	  for (var i = 0; i < arr.length-1; i++) {
-		  var splitted = arr[i].split(",").map(d => d.substring(1, d.length-1))
-		  var word = splitted[0]
-		  var count = parseInt(splitted[1])
-		  if(count > 10 && word.length > 2)
-		  words.push({text:word,size:Math.round(count/2)})
-	  }
+fetch('papers.csv')
+	.then(response => response.text())
+	.then(load_papers)
+	.then(response => response.text())
+	.then(load_data)
 
-var layout = cloud()
-    .size([1000, 800])
-    .words(words)
-    .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Impact")
-    .fontSize(function(d) { return d.size; })
-    .on("end", draw);
-
-
-layout.start();
-
-function draw(words) {
-  d3.select("body").append("svg")
-      .attr("width", layout.size()[0])
-      .attr("height", layout.size()[1])
-    .append("g")
-      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-    .selectAll("text")
-      .data(words)
-    .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
-      .style("font-family", "Impact")
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
+function load_papers(data){
+	arr = data.split("\n");
+	for (var i = 0; i < arr.length-1; i++) {
+		var splitted = arr[i].split(",")
+		var html = splitted.slice(1).join(",");
+		html = html.substring(1,html.length-2);
+		//html = html.replace(/""/g,'\"'); not needed?
+		papers[splitted[0]] = html
+	}
+	return fetch('freq_data.csv')
 }
 
-})
+function load_data(data){
+	arr = data.split("\n");
+	words = [];
+	for (var i = 0; i < arr.length-1; i++) {
+		var splitted = arr[i].split(",");
+		var word = splitted[0].substring(1,splitted[0].length-1);//remove quotes
+		var count = parseInt(splitted[1]);
+		var related = splitted.slice(2).join(",");
+		related_papers = related.substring(2,related.length-2);//remove "{}"
+		if(count > 10 && word.length > 2){
+			words.push({text:word,size:Math.ceil(count/10),related: related_papers});
+		}
+	}
+	//Draw Wordcloud
+	var layout = cloud()
+		.size([1000, 800])
+		.words(words)
+		.padding(5)
+		.rotate(function() { return ~~(Math.random() * 1) * 90; })
+		.font("Impact")
+		.fontSize(function(d) { return d.size; })
+		.on("end", draw);
+	layout.start();
+
+
+
+
+	function draw(words) {
+	  d3.select("body").append("svg")
+		  .attr("width", layout.size()[0])
+		  .attr("height", layout.size()[1])
+		.append("g")
+		  .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+		.selectAll("text")
+		  .data(words)
+		.enter().append("text")
+		  .style("font-size", function(d) { return d.size + "px"; })
+		  .style("font-family", "Impact")
+		  .attr("text-anchor", "middle")
+		  .attr("transform", function(d) {
+			return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+		  })
+		  .text(function(d) { return d.text; })
+		 .on("click",show_related);
+	}
+}
+
+function show_related(d,i){
+	var data = []
+	var pairs = d.related.split(", ")
+	for (var i = 0;i < pairs.length - 1; i++) {
+		kv = pairs[i].split(": ");
+		data.push(kv[1]+papers[kv[0]]);
+	}
+	d3.select('body').select('ul').remove();
+	var ul = d3.select('body').append('ul');
+	ul.selectAll('li')
+	.data(data)
+	.enter()
+	.append('li')
+	.html(String);
+}
 
 },{"d3":34,"d3-cloud":6}],2:[function(require,module,exports){
 // https://d3js.org/d3-array/ v1.2.4 Copyright 2018 Mike Bostock
