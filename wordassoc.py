5a147156ce22cdb85a36148e75b128bd63f75e3d
ben <benjaminhodges07@gmail.com>
2019-08-03 20:30:54 +0000

    fixed wordassoc

--- wordassoc.py
+++ wordassoc.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import numpy as np
 import scipy.sparse as sp
 import csv
@@ -7,21 +6,26 @@ from sys import argv
 
 X = sp.load_npz('tfs.npz')
 vocab = np.load('vocab.npy', allow_pickle=True).item()
-inverse = {v:k for k,v in vocab.items()}
 limit = 20
 
 if len(argv) == 1 or argv[1] not in vocab:
     exit()
 
 word = vocab[argv[1]]
-s  = X[:,word].toarray().ravel().argsort(axis=0)[:limit]
-papers = X[s,word].nonzero()[0]
+column = X[:,word].nonzero()[0]
+papers = (-column).argsort()[:limit]
 
-with open("papers.csv","r",encoding='utf-8') as f:
+htmldict = dict()
+with open("papers.csv","r", encoding='utf-8') as f:
     reader = csv.reader(f)
-    print("<ul>",end="")
     for paper,html,_ in reader:
         paper = int(paper)
         if paper in papers:
-            print(f"<li>[{X[paper,word]} hits] {html}</li>",end="")
-    print("<ul>",end="")
+            htmldict[paper] = html
+            
+print("<ul>",end="")
+for paper in papers:
+    count = column[paper]
+    html = htmldict[paper]
+    print(f"<li>[{count} hits] {html}</li>",end="")
+print("</ul>",end="")
