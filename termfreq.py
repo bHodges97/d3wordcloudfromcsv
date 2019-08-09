5b22a5e13a06982c98e528225fba882e565174cf
ben <benjaminhodges07@gmail.com>
2019-08-09 10:50:55 +0000

    switched to using reverse map

--- termfreq.py
+++ termfreq.py
@@ -5,27 +5,27 @@ import scipy.sparse as sp
 import re
 from sys import argv
 
+limit = 1000
 X = sp.load_npz('tfs.npz')
 vocab = np.load('vocab.npz', allow_pickle=True)['arr_0'].item()
-inverse = {v:k for k,v in vocab.items()}
-limit = 1000
+
 if len(argv) == 1 or argv[1] == "":#No word specified
     tfs = np.asarray(X.sum(axis=0)).ravel()
     indices = (-tfs).argsort()[:limit]
     for i in indices:
-        print(f"{inverse[i]}, {tfs[i]}")
+        print(f"{vocab[i]}, {tfs[i]}")
 else:
     words = []
     word = argv[1].strip()
     pattern = re.compile(word)
-    for k,v in vocab.items():
-        if pattern.match(k):
-            words.append(v)
+    for idx,word in vocab.items():
+        if pattern.match(word):
+            words.append(idx)
     if words:
         papers = np.unique(X[:,words].nonzero()[0])
         tfs = np.asarray(X[papers,:].sum(axis=0)).ravel()
         indices = (-tfs).argsort()[:limit]
         for i in indices:
-            print(f"{inverse[i]}, {tfs[i]}")
+            print(f"{vocab[i]}, {tfs[i]}")
     else:
         print("Not found,1")
